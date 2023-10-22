fad247a87aa4cb27a8337cd2a2000bb42eae2452
YuriyV86 <vyuriks@gmail.com>
2023-10-22 18:37:48 +0300

    react-1

---
+++ src/component/box/index.js
@@ -0,0 +1,7 @@
+import "./index.css"
+
+export default function Box({children, className, shadow }) {
+  return <div className={`box ${shadow ? "box--shadow" : ""} ${className}`}>
+    {children}
+  </div>
+} 
